272c0f141f4381ee221d4d9542b30b80713bb703
dev <davidboles@davids-MBP.att.net>
2015-12-29 15:15:24 -0800

    committing to github

---
+++ My Hood/DataService.swift
@@ -0,0 +1,82 @@
+//
+//  DataService.swift
+//  My Hood
+//
+//  Created by dev on 12/28/15.
+//  Copyright © 2015 ciccio boles. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class DataService {
+    static let instance = DataService()
+    let KEY_POSTS = "posts"
+    private var _loadedPosts = [Post]()
+    
+    var loadedPosts: [Post] {
+        return _loadedPosts
+    }
+    
+    func savePosts() {
+        let postsData = NSKeyedArchiver.archivedDataWithRootObject(_loadedPosts)
+        NSUserDefaults.standardUserDefaults().setObject(postsData, forKey: KEY_POSTS)
+        NSUserDefaults.standardUserDefaults().synchronize() 
+        
+    }
+
+    func loadPosts() {
+        if let postsData = NSUserDefaults.standardUserDefaults().objectForKey(KEY_POSTS) as? NSData{
+            if let postsArray = NSKeyedUnarchiver.unarchiveObjectWithData(postsData) as? [Post] {
+                _loadedPosts = postsArray
+                
+            }
+        }
+        
+        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "postsLoaded", object: nil))
+        
+    }
+
+    func saveImageAndCreatePath(image: UIImage) ->String {
+        let imgData = UIImagePNGRepresentation(image)
+        let imgPath = "\(NSDate.timeIntervalSinceReferenceDate()).png"
+        let fullPath = documentsPathForFileName(imgPath)
+        imgData?.writeToFile(fullPath, atomically: true)
+        return imgPath
+    }
+
+    func imageForPath(path: String) -> UIImage? {
+        let fullPath = documentsPathForFileName(path)
+        let image = UIImage(named: fullPath)
+        return image
+        
+    }
+
+    func addPost(post: Post) {
+        _loadedPosts.append(post)
+        savePosts()
+        loadPosts()
+        
+    }
+    
+    
+    func documentsPathForFileName(name: String) ->String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let fullPath = paths [0] as NSString
+        return fullPath.stringByAppendingPathComponent(name)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}//
---
+++ My Hood/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  My Hood
+//
+//  Created by dev on 12/28/15.
+//  Copyright © 2015 ciccio boles. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        postImg.layer.cornerRadius = postImg.frame.size.width / 2
+        postImg.clipsToBounds = true
+      }
+   
+    func configureCell(post: Post) {
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+        postImg.image = DataService.instance.imageForPath(post.imagePath)
+    }
+    
+    
+
+}
